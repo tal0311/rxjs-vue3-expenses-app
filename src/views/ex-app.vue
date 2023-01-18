c6ac320a7b12ee564071178ff6e053e807ac2521
tal0311 <tal.amit0311@gmail.com>
2023-01-18 09:23:57 +0200

    service and list

--- src/views/ex-app.vue
+++ src/views/ex-app.vue
@@ -1,13 +1,33 @@
 <template>
- <section class="ex-app">
+ <section v-if="items" class="ex-app">
   <h1>app</h1>
+  <ul>
+   <li v-for="item in items" :key="item.id">
+    {{ item.content }}
+   </li>
+  </ul>
   <RouterView />
  </section>
 </template>
 
 <script>
+import { itemService } from './../services/item.service.js'
 export default {
- name: 'ex-app'
+ name: 'ex-app',
+ created() {
+  itemService.query()
+  itemService.ex$.subscribe((items) => {
+   this.items = items
+  })
+ },
+ data() {
+  return {
+   items: null
+  }
+ },
+ methods: {
+
+ }
 }
 </script>
 
