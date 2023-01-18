50324973a4e9d8db869c9f77015b865f68411c44
tal0311 <tal.amit0311@gmail.com>
2023-01-19 01:29:37 +0200

    user-settings basic layout

--- src/views/user-settings.vue
+++ src/views/user-settings.vue
@@ -1,12 +1,43 @@
 <template>
- <section class="user-settings">
-  <h1>user-settings</h1>
+ <section class="user-settings grid">
+  <button class="icon close-modal-btn" @click="closeModal" v-icon="'times'"></button>
+  <h1 class="modal-header flex">user-settings</h1>
+  <!-- <div class="modal-img">
+   <img src="src/assets/img/pngwing.com.png" alt="">
+  </div> -->
+  <form class="modal-content flex">
+   <label for="username">username</label>
+   <input type="text" name="username">
+   <label for="account-name">account name</label>
+   <input type="text" name="account-name">
+   <label for="account-number">
+    account number
+   </label>
+   <input type="number" name="account-name">
+
+   <label for="language">
+    language
+   </label>
+   <select name="language" id="">
+    <option value="en">English</option>
+    <option value="he">עברית</option>
+   </select>
+  </form>
+  <div class="modal-actions flex align-center">
+   <button>save changes</button>
+  </div>
  </section>
 </template>
 
 <script>
 export default {
- name: 'user-settings'
+ name: 'user-settings',
+ methods: {
+  closeModal() {
+   console.log('this.$router:', this.$router)
+   this.$router.go(-1)
+  },
+ }
 }
 </script>
 
