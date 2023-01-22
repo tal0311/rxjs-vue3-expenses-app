55b8d2d2d317ced2d62c38c8334d005c8da199a0
tal0311 <tal.amit0311@gmail.com>
2023-01-22 17:57:22 +0200

    helper

--- src/components/app-helper.vue
+++ src/components/app-helper.vue
@@ -1,15 +1,45 @@
 <template>
- <section class="app-helper">
-  <h3>app appHelper</h3>
-  <p>content</p>
+  <section @click="toggle" :class="[isOpen && 'open', isNotified && 'notify', 'app-helper']">
+    <button class="icon" v-icon="'help'"></button>
+    <section v-if="isOpen" class="content">
 
+      <h3>{{ header }}</h3>
+      <p>{{ content }}</p>
 
- </section>
+    </section>
+
+  </section>
 </template>
 
 <script>
+import { getHelperContent } from './../services/helper.service.js'
 export default {
- name: 'app-helper',
+  name: 'app-helper',
+  data() {
+    return {
+      isOpen: false,
+      isNotified: false,
+      content: '',
+      header: ''
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+      this.isNotified = false;
+    }
+  },
+  watch: {
+    $route: function (to, from) {
+      console.log('to :', to)
+      const { content, header } = getHelperContent(to)
+      this.content = content;
+      this.header = header;
+      this.isNotified = true;
+
+    },
+    deep: true
+  }
 }
 </script>
 
---
+++ src/services/helper.service.js
@@ -0,0 +1,28 @@
+
+
+
+
+export function getHelperContent(to) {
+ console.log('to.name:', to.name)
+
+ return content[to.name] || content['default']
+
+}
+
+const content = {
+ 'settings': {
+  content: 'In the user settings we can change the main features the user works with.Username, color, language and more....',
+  header: 'User Settings'
+ },
+ 'app': {
+  content: 'On this page the user performs the main actions in the application.Adds, updates, deletes and changes the order of expenses.In addition, you can search by name and by category.',
+  header: 'Expenses Page'
+ },
+ 'home': {
+  content: 'It is a management application to track the user\'s expenses and income.In the navigation bar you can edit expenses or edit the user settings.',
+  header: 'Home Page'
+ }
+}
+
+
+
