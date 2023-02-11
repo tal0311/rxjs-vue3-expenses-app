60ac1d415ea4a515c825852d3e37bdb094d7f4ed
tal0311 <tal.amit0311@gmail.com>
2023-02-11 19:03:48 +0200

    title attr tooltip

--- src/components/ex-preview.vue
+++ src/components/ex-preview.vue
@@ -1,67 +1,68 @@
 <template>
- <article :class="['ex-preview', item.category]" :style="borderStyle">
-  <div class="cell">
-   <p>{{ item.content }}</p>
-  </div>
-  <div class="cell">
-   <p>{{ item.amount }}</p>
-  </div>
-  <div class="cell">
-   <p>{{ getDate }}</p>
-  </div>
-  <div class="cell">
-   <div :class="['actions-container flex', isActions && 'relative']">
-    <i class="icon" v-icon="'options'" v-if="!isActions" @click="isActions = !isActions">actions</i>
-    <section v-else :style="getStyle">
-     <i v-for="action in actions" :class="action.icon" @click="$emit('action', { type: action.icon, itemId: item.id })"
-      v-icon="action.icon" :key="action.icon"></i>
-    </section>
-   </div>
-  </div>
- </article>
+  <article :class="['ex-preview', item.category]" :data-title="item.category" :style="borderStyle">
+    <div class="cell">
+      <p>{{ item.content }}</p>
+    </div>
+    <div class="cell">
+      <p>{{ item.amount }}</p>
+    </div>
+    <div class="cell">
+      <p>{{ getDate }}</p>
+    </div>
+    <div class="cell">
+      <div :class="['actions-container flex', isActions && 'relative']">
+        <i class="icon" v-icon="'options'" v-if="!isActions" @click="isActions = !isActions">actions</i>
+        <section v-else :style="getStyle">
+          <i v-for="action in actions" :class="action.icon"
+            @click="$emit('action', { type: action.icon, itemId: item.id })" v-icon="action.icon"
+            :key="action.icon"></i>
+        </section>
+      </div>
+    </div>
+  </article>
 </template>
 
 <script>
 import { setCatColor } from '../services/category.service';
 export default {
- name: 'ex-preview',
- emits: ['action'],
- props: {
-  item: Object
- },
- data() {
-  return {
-   isActions: false,
-   actions: [
-    { icon: 'edit', title: 'Edit item' },
-    { icon: 'archive', title: 'Archive item' },
-    { icon: 'trash', title: 'Remove item' },
-   ]
-  }
- },
- methods: {
- },
- computed: {
-  getDate() {
-   return new Date(this.item.createdAt).toLocaleDateString()
+  name: 'ex-preview',
+  emits: ['action'],
+  props: {
+    item: Object
   },
-  borderStyle() {
-   return {
-    'border-Left': `5px solid ${setCatColor(this.item.category)}`
-   }
-  },
-  getStyle() {
-   if (this.isActions) {
+  data() {
     return {
-     'position': 'absolute',
-     'display': 'flex',
-     'right': '0',
-     'transform': 'translateY(-60%)',
-
+      isActions: false,
+      actions: [
+        { icon: 'edit', title: 'Edit item' },
+        { icon: 'archive', title: 'Archive item' },
+        { icon: 'trash', title: 'Remove item' },
+      ]
     }
-   }
+  },
+  methods: {
+  },
+  computed: {
+    getDate() {
+      return new Date(this.item.createdAt).toLocaleDateString()
+    },
+    borderStyle() {
+      return {
+        'border-Left': `5px solid ${setCatColor(this.item.category)}`
+      }
+    },
+    getStyle() {
+      if (this.isActions) {
+        return {
+          'position': 'absolute',
+          'display': 'flex',
+          'right': '0',
+          'transform': 'translateY(-60%)',
+
+        }
+      }
+    }
   }
- }
 }
 
 </script>
